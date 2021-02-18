56f7f945e367067c2bfa1a6f3eee42c572a599a4
anindo69 <53431453+anindo69@users.noreply.github.com>
2021-02-18 09:51:56 +0600

    Add files via upload

---
+++ code3.java
@@ -0,0 +1,13 @@
+public class code3 {
+
+    public static void main(String[] args) {
+        int rslt = 69;
+        String mark = (rslt>=80) ? "u got A+" : "sorry try nxt time harder";
+        System.out.println(mark);
+
+
+
+      
+       
+    }
+}
